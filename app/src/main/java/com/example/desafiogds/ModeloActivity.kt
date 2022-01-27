ea21a2c221f9817a821dad056aa6fc9f60becb01
Bianca <bianca-franco1@hotmail.com>
2022-01-27 18:03:26 -0300

    programação botões voltar

--- app/src/main/java/com/example/desafiogds/ModeloActivity.kt
+++ app/src/main/java/com/example/desafiogds/ModeloActivity.kt
@@ -1,11 +1,26 @@
 package com.example.desafiogds
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desafiogds.databinding.ActivityMarcaBinding
+import com.example.desafiogds.databinding.ActivityModeloBinding
 
 class ModeloActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityModeloBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modelo)
+        setContentView(R.layout.activity_marca)
+        binding= ActivityModeloBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnVoltarModelo.setOnClickListener {
+            voltarModelo()
+        }
+    }
+    private fun voltarModelo(){
+        val voltarTela = Intent (this, HomeActivity::class.java)
+        startActivity(voltarTela)
     }
 }
--- app/src/main/java/com/example/desafiogds/PlacaActivity.kt
+++ app/src/main/java/com/example/desafiogds/PlacaActivity.kt
@@ -1,11 +1,26 @@
 package com.example.desafiogds
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desafiogds.databinding.ActivityModeloBinding
+import com.example.desafiogds.databinding.ActivityPlacaBinding
 
 class PlacaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPlacaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placa)
+        setContentView(R.layout.activity_marca)
+        binding= ActivityPlacaBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnVoltarPlaca.setOnClickListener {
+            voltarPlaca()
+        }
+    }
+    private fun voltarPlaca(){
+        val voltarTela = Intent (this, HomeActivity::class.java)
+        startActivity(voltarTela)
     }
 }
